90dea9ffb3b91b2138562907cff9d2229ebbf2d6
piokal4040 <150016447+piokal4040@users.noreply.github.com>
2025-12-02 13:54:41 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,10 +11,10 @@ provider "azurerm" {
   features {}
 }
 
-#resource "azurerm_resource_group" "example" {
-#  name     = var.resource_group_name
-#  location = var.location
-#}
+resource "azurerm_resource_group" "example" {
+ name     = var.resource_group_name
+ location = var.location
+}
 
 resource "azurerm_service_plan" "example" {
   name                = var.app_service_plan_name
