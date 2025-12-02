45f6f521d074f3c8cbc06422fb6c138caa4bb7bb
piokal4040 <150016447+piokal4040@users.noreply.github.com>
2025-12-02 14:57:09 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,7 +2,7 @@
 variable "resource_group_name" {
 description = "The name of the resource group"
 type        = string
-default     = "dfdfdfdfddvfhjujmnbs12_43"
+default     = "dfdfdfdfddvfhjujmnbs12_4333"
 }
 
 
