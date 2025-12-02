8a9d4bf0d9990a2b41c545ba67fd0d2eac563c76
piokal4040 <150016447+piokal4040@users.noreply.github.com>
2025-12-02 13:31:17 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,13 +1,13 @@
 variable "resource_group_name" {
   description = "The name of the resource group"
   type        = string
-  default     = "example-resources"
+  default     = "PKexample-resources"
 }
 
 variable "location" {
   description = "The Azure region where resources will be created"
   type        = string
-  default     = "East US"
+  default     = "uksouth"
 }
 
 variable "app_service_plan_name" {
